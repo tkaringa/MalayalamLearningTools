0b895f016540c8d0709aefbdc509c28af77628f6
tkaringa <111993596+tkaringa@users.noreply.github.com>
2022-09-13 15:21:23 -0400

    Updates main.java
    Changes print statement to be more designed for the input of actual pronunciation

--- LetterLearner/Main.java
+++ LetterLearner/Main.java
@@ -7,13 +7,13 @@ public class Main {
         int letterIndex = 0;
         do  {
             letterIndex = (int) Math.floor(Math.random() * 4); //creates a random index to find a letter in the malayalam array
-            System.out.println("Please type " + "'" + MalayalamLetters.getLetter(letterIndex) + "': ");
+            System.out.println("Please type how you would pronounce " + "'" + MalayalamLetters.getLetter(letterIndex) + "': ");
             letterAnswer = in.nextLine();
             while(!MalayalamLetters.isLetterCorrect(letterIndex, letterAnswer)) {
                 System.out.println("Sorry, that wasn't right, try again: ");
                 letterAnswer = in.nextLine();
             }
-            System.out.println("Awesome job! Keep going, press 'ENTER' to continue, or 'stop' to terminate program");
+            System.out.println("Awesome job! Keep going, press any key to continue, or type 'stop' to terminate program");
         } while (!(in.nextLine().equals("stop")));
     }
 }
