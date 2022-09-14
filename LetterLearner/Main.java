b9236923334dc7349f4f23b68ca9b7fe0759d36c
tkaringa <111993596+tkaringa@users.noreply.github.com>
2022-09-13 20:13:00 -0400

    Update Main.java
    adds the function to reveal an answer if the user is stuck on the pronunciation of a letter

--- LetterLearner/Main.java
+++ LetterLearner/Main.java
@@ -6,12 +6,21 @@ public class Main {
         String letterAnswer = "";
         int letterIndex = 0;
         do  {
-            letterIndex = (int) Math.floor(Math.random() * 4); //creates a random index to find a letter in the malayalam array
-            System.out.println("Please type how you would pronounce " + "'" + MalayalamLetters.getLetter(letterIndex) + "': ");
+            letterIndex = (int) Math.floor(Math.random() * 50); //creates a random index to find a letter in the malayalam array
+            System.out.println("Please type how you would pronounce " + "'" + MalayalamLetters.getLetter(letterIndex) + "' (type 'reveal' if you want the answer): ");
             letterAnswer = in.nextLine();
+            if(letterAnswer.equals("reveal")) {
+                System.out.println(MalayalamLetters.getPronunciation(letterIndex));
+            }
             while(!MalayalamLetters.isLetterCorrect(letterIndex, letterAnswer)) {
-                System.out.println("Sorry, that wasn't right, try again: ");
+                System.out.println("Try '" + MalayalamLetters.getLetter(letterIndex) + "' again (type 'reveal' if you want the answer): ");
                 letterAnswer = in.nextLine();
+                if(letterAnswer.equals("reveal")) {
+                    System.out.println(MalayalamLetters.getPronunciation(letterIndex));
+                }
+            }
+            if(letterAnswer.equals("reveal")) {
+                System.out.println(MalayalamLetters.getPronunciation(letterIndex));
             }
             System.out.println("Awesome job! Keep going, press any key to continue, or type 'stop' to terminate program");
         } while (!(in.nextLine().equals("stop")));
